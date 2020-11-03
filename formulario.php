0418820cb8bdde1522a81fd56464d40f1b866c99
David Valenzuela <2077157@gmail.com>
2020-11-03 14:32:12 +0100

    Inicialización

---
+++ formulario.php
@@ -0,0 +1,10 @@
+<html>
+
+<body>
+<form action="formulario.php" method="POST">
+    <label for="numero">Introduce el número</label>
+    <input type="text" id="numero" name="numero" />
+
+</form>
+</body>
+</html>
